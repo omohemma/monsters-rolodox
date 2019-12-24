2999a05c88f64cc3c846a709cdd6ab0c6c8ee342
omohemma <emmaomoh@gmail.com>
2019-12-24 12:43:52 +0100

    comment cardlist component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./card-list.styles.css";
 
+//Functional approach to creating react components
 export const CardList = props => {
-  return <div className='card-list'>{props.children}</div>;
+  return <div className="card-list">{props.children}</div>;
 };
