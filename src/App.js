0151f1c5b63c1d2c157ae7ade5140f7c9feb79bc
omohemma <emmaomoh@gmail.com>
2019-12-24 12:42:17 +0100

    Commenting Code

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import "./App.css";
 
 class App extends Component {
   constructor() {
+    // instatiate Constructor
     super();
 
+    //Initial state of component
     this.state = {
       monsters: []
     };
   }
 
+  //When page mount on the DOM
+
   componentDidMount() {
+    /*Consume Extrenal APIs
+      Format promise int JSON
+      Update state of monster with response from APIs
+     */
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
