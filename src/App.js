4404f5b7d5ff85f0b614789fd921f4198ec128a8
omohemma <emmaomoh@gmail.com>
2019-12-27 19:15:12 +0100

    style cardlist and added new comment to code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
+//Class 'App' extends React.Component Library
 class App extends Component {
   constructor() {
     // instatiate Constructor
+    //This is done to manage the state of a component
     super();
 
     //Initial state of component
@@ -13,22 +15,23 @@ class App extends Component {
     };
   }
 
-  //When page mount on the DOM
-
+  //When page mount on the DOM [Lifecycle Hooks]
   componentDidMount() {
-    /*Consume Extrenal APIs
-      Format promise int JSON
-      Update state of monster with response from APIs
+    /*Consume Extrenal APIs using fetch method
+      Format promise into JSON
+      Update state of monster with response from APIs using setState Fuction 
+      which mutates initialized state
      */
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
 
+  // React Component receieves its state(monsters) in an attribue-like form called props
   render() {
     return (
       <div className="App">
-        <CardList monsters = {this.state.monsters}/>>
+        <CardList monsters={this.state.monsters} />>
       </div>
     );
   }
