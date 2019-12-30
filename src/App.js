5b60949a44e5f74f2994d34fc175eb686d746852
omohemma <emmaomoh@gmail.com>
2019-12-30 11:23:18 +0100

    filter monsters

--- src/App.js
+++ src/App.js
@@ -30,18 +30,20 @@ class App extends Component {
 
   // React Component receieves its state(monsters) in an attribue-like form called props
   render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
         <input
           type="search"
           placeholder="search monsters"
-          onChange={e => {
-            this.setState({ searchField: e.target.value }, () => {
-              console.log(this.state);
-            });
-          }}
+          onChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
