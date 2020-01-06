e19594b9f13f15269f089368e3f2d97fd0ee14c7
omohemma <emmaomoh@gmail.com>
2020-01-06 13:28:37 +0100

    Updates

---
+++ precache-manifest.5795c24d0d44782354564536be0719c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c589729ec0f211dc1ee4375dab776103",
+    "url": "/monsters-rolodox/index.html"
+  },
+  {
+    "revision": "cd084cd308d6c4539fda",
+    "url": "/monsters-rolodox/static/css/main.9d8f9885.chunk.css"
+  },
+  {
+    "revision": "c235b627b078c3f50ff4",
+    "url": "/monsters-rolodox/static/js/2.09f5a5bc.chunk.js"
+  },
+  {
+    "revision": "cd084cd308d6c4539fda",
+    "url": "/monsters-rolodox/static/js/main.836443d3.chunk.js"
+  },
+  {
+    "revision": "f174873bd8f8e10a7896",
+    "url": "/monsters-rolodox/static/js/runtime-main.531970c8.js"
+  }
+]);
